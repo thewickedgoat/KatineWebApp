9988595ce48dc689a1cbcc7d6dfa7c0801862655
Fuflino <St@networks.dk>
2017-03-27 12:02:43 +0200

    Packages (material, fontawesome m.m. added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-awesome';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { TodaysMenuComponent } from './menu/todays-menu/todays-menu.component';
-import { MenuComponent } from './menu/menu.component';
 import { MenuItemComponent } from './menu/menu-item/menu-item.component';
 import { CreateMenuComponent } from './menu/create-menu/create-menu.component';
 import { EditMenuComponent } from './menu/edit-menu/edit-menu.component';
@@ -14,7 +18,6 @@ import { EditMenuComponent } from './menu/edit-menu/edit-menu.component';
   declarations: [
     AppComponent,
     TodaysMenuComponent,
-    MenuComponent,
     MenuItemComponent,
     CreateMenuComponent,
     EditMenuComponent
@@ -22,7 +25,10 @@ import { EditMenuComponent } from './menu/edit-menu/edit-menu.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    Angular2FontAwesomeModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
