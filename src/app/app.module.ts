952e51e30cc372f559def097f90fa2681a87d14e
Fuflino <St@networks.dk>
2017-04-03 09:11:21 +0200

    Firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import {MaterialModule} from '@angular/material';
 import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-awesome';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {AngularFireModule, AuthMethods, AuthProviders} from 'angularfire2';
 
 import 'hammerjs';
 
@@ -14,6 +15,21 @@ import { MenuItemComponent } from './menu/menu-item/menu-item.component';
 import { CreateMenuComponent } from './menu/create-menu/create-menu.component';
 import { EditMenuComponent } from './menu/edit-menu/edit-menu.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBbmu27G8I-DqbGNlJQBEWJPmL9IcoYr6M',
+  authDomain: 'firstangularproject-deef3.firebaseapp.com',
+  databaseURL: 'https://firstangularproject-deef3.firebaseio.com',
+  projectId: 'firstangularproject-deef3',
+  storageBucket: 'firstangularproject-deef3.appspot.com',
+  messagingSenderId: '643731449610'
+};
+
+export const fireBaseLoginConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+};
 
 @NgModule({
   declarations: [
@@ -22,7 +38,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     MenuItemComponent,
     CreateMenuComponent,
     EditMenuComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +47,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     HttpModule,
     MaterialModule,
     Angular2FontAwesomeModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(firebaseConfig, fireBaseLoginConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
