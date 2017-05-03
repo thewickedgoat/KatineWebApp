f6faa7e28614549a401229ce36254d02c3602391
Raakost <jeanettesydboege@gmail.com>
2017-05-03 12:13:15 +0200

    Menu history page design

--- src/app/menu/menu.service.ts
+++ src/app/menu/menu.service.ts
@@ -8,17 +8,24 @@ export class MenuService implements OnInit {
   menu: Menu[];
   dish: Dish[];
 
+
   constructor() {
     this.dish = [{
       id: 1,
-      name: 'dish1',
-      image: 'image1'
+      name: 'Råkost salat',
+      image: 'http://res.cloudinary.com/bjoernebanden/image/upload/v1493487117/ra_CC_8Akost-med-spidska_CC_8Al-og-guler_C3_B8dder_lyyrhu.jpg'
     },
       {
         id: 2,
-        name: 'dish2',
-        image: 'image2'
+        name: 'Klar suppe',
+        image: 'http://res.cloudinary.com/bjoernebanden/image/upload/v1493482217/klar-suppe-med-k_C3_B8d-og-melboller-23_tajljq.jpg'
+      },
+      {
+        id: 3,
+        name: 'Stegt Flæsk',
+        image: 'http://res.cloudinary.com/bjoernebanden/image/upload/v1493482190/restaurant-bennys-aarhus-080515-2221262-regular_mmb71l.jpg'
       }];
+
     this.menu = [{
       id: 1,
       date: Date.now(),
@@ -35,6 +42,6 @@ export class MenuService implements OnInit {
   }
 
   getMenu(): Menu[] {
-  return this.menu;
+    return this.menu;
   }
 }
